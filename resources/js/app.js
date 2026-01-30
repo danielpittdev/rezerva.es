1f22a3c4c9f2d1d6e0172f22f4782e14a951d400
Daniel <dgonzalezpitt@safareyes.es>
2026-01-30 16:06:36 +0100

    cambios

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,9 @@ import '@tailwindplus/elements';
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 
-import 'preline';
-import ApexCharts from 'apexcharts';
-window.ApexCharts = ApexCharts;
+// import 'preline';
+// import ApexCharts from 'apexcharts';
+// window.ApexCharts = ApexCharts;
 
 import FormBackendValidation from 'form-backend-validation';
 
--- resources/views/panel/reservas.blade.php
+++ resources/views/panel/reservas.blade.php
@@ -3,9 +3,7 @@
 @section('contenido')
    <section class="grid h-full grid-cols-[auto_1fr_1fr] grid-rows-2 gap-2 grid-rows-1">
       <div class="row-span-2 bg-base-300 p-3 rounded-lg relative box border border-base-content/10 w-xs overflow-y-auto">
-         <div class="caja h-[3000px]">
-            izquierda
-         </div>
+
       </div>
 
       <div class="bg-base-300 p-3 rounded-lg relative box border border-base-content/10 overflow-y-auto">
