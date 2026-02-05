f009a9c124f020bd87838029fea238e2ac0c5337
Daniel <dgonzalezpitt@safareyes.es>
2026-02-05 13:59:41 +0100

    Cambios en VITE

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
                 "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/js/request-manager.js",
-                "resources/js/form-handler.js",
                 "resources/js/calendario.js",
             ],
             refresh: true,
